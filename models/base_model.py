9301f17ba1778e7b1917b4c073cf259573a6f81c
Zelipha <zeliphawambui756@gmail.com>
2022-01-30 10:25:17 -0800

    Modified file

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.today()
             models.storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
