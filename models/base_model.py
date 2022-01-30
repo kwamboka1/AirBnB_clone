fd44ea06cd0b698e0aaa3a0f7552975627b3dbb4
Zelipha <zeliphawambui756@gmail.com>
2022-01-30 10:43:49 -0800

    Modified file

--- models/base_model.py
+++ models/base_model.py
@@ -16,18 +16,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializing all attributes
         """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            models.storage.new(self)
-        else:
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+        if len(kwargs) != 0:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(kwargs[key], f)
                 if key != '__class__':
                     setattr(self, key, value)
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """
